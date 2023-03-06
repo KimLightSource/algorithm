6110fe7a9aa1f2aefe884ac61cb5d3c850a525aa
KimLightSource <qkek875@gmail.com>
2023-03-06 10:33:52 +0900

    자바 알고리즘 4. 단어뒤집기

--- Main.java
+++ Main.java
@@ -1,22 +1,37 @@
+import java.sql.Array;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-    public int solution(String str, char t) {
-        int answer = 0;
-        str = str.toUpperCase();
-        t = Character.toUpperCase(t);
-        for (char c : str.toCharArray()) {
-            if(c == t) answer ++;
+    public static void main(String[] args){
+        Main main = new Main();
+        Scanner in=new Scanner(System.in);
+        int n = in.nextInt();
+        String[] str = new String[n];
+        for(int i=0; i<n; i++) {
+            str[i] = in.next();
         }
-        return answer;
+        for (String x : main.solution(n, str)) {
+            System.out.println(x);
+        }
+        return ;
     }
 
-    public static void main(String[] args){
-        Main T = new Main();
-        Scanner kb=new Scanner(System.in);
-        String str = kb.next();
-        char c = kb.next().charAt(0);
-        System.out.println(T.solution(str, c));
-        return ;
+    public ArrayList<String> solution(int n, String[] str) {
+        ArrayList<String> answer = new ArrayList<>();
+        for (String x : str) {
+            char[] s=x.toCharArray();
+            int lt=0, rt=x.length()-1;
+            while (lt < rt) {
+                char tmp=s[lt];
+                s[lt]=s[rt];
+                s[rt]=tmp;
+                lt++;
+                rt--;
+            }
+            String tmp = String.valueOf(s);
+            answer.add(tmp);
+        }
+        return answer;
     }
 }
