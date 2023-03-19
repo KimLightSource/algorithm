a1bc2368dfe27907e8d916b2c2e82d9520bb5bd2
KimLightSource <qkek875@gmail.com>
2023-03-19 17:12:14 +0900

    1157번 : 단어공부

--- Main.java
+++ Main.java
@@ -1,22 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.sql.Array;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
-        Main main = new Main();
-        Scanner in=new Scanner(System.in);
-        String word = in.next();
-        System.out.println(main.solution(word));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String word = br.readLine();
+        int[] arr = new int[26];
+        word = word.toUpperCase();
+        for (int i = 0; i<word.length(); i++) {
+            if ('A' <= word.charAt(i) && word.charAt(i) <= 'Z') {
+                arr[word.charAt(i) - 'A']++;
+            }
+        }
+        int max = -1;
+        char ch = '?';
+        for (int i = 0; i < 26; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+                ch = (char) (i + 65);
+            } else if (arr[i] == max) {
+                ch = '?';
+            }
+        }
+        System.out.print(ch);
     }
 
-    public int solution(String word) {
-        int answer = 0;
-        StringBuilder sb = new StringBuilder(word);
-        String s = sb.reverse().toString();
-        if (word.equals(s)) {
-            answer = 1;
-        }
-        return answer;
-    }
 }
