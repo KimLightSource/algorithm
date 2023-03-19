50e26ef62736bff394f506a9abe2b3eaa2362ade
KimLightSource <qkek875@gmail.com>
2023-03-19 16:09:00 +0900

    10988번

--- Main.java
+++ Main.java
@@ -6,31 +6,16 @@ public class Main {
     public static void main(String[] args){
         Main main = new Main();
         Scanner in=new Scanner(System.in);
-        int n = in.nextInt();
-        String[] str = new String[n];
-        for(int i=0; i<n; i++) {
-            str[i] = in.next();
-        }
-        for (String x : main.solution(n, str)) {
-            System.out.println(x);
-        }
-        return ;
+        String word = in.next();
+        System.out.println(main.solution(word));
     }
 
-    public ArrayList<String> solution(int n, String[] str) {
-        ArrayList<String> answer = new ArrayList<>();
-        for (String x : str) {
-            char[] s=x.toCharArray();
-            int lt=0, rt=x.length()-1;
-            while (lt < rt) {
-                char tmp=s[lt];
-                s[lt]=s[rt];
-                s[rt]=tmp;
-                lt++;
-                rt--;
-            }
-            String tmp = String.valueOf(s);
-            answer.add(tmp);
+    public int solution(String word) {
+        int answer = 0;
+        StringBuilder sb = new StringBuilder(word);
+        String s = sb.reverse().toString();
+        if (word.equals(s)) {
+            answer = 1;
         }
         return answer;
     }
